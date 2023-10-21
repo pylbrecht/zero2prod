6cb0acd658dc4efc38b94c3f2284bb797a52125f
pylbrecht <pylbrecht@mailbox.org>
2023-10-21 10:02:19 +0200

    Spawn server with random port in test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 //! main.rs
 
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
