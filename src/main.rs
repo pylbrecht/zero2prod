08050c6fe8b3e0c265ca22742ad3411a95c39f21
pylbrecht <pylbrecht@mailbox.org>
2023-11-02 14:40:06 +0100

    Persist a new subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! main.rs
 
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -10,8 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection = PgConnection::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect Postgres.");
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
