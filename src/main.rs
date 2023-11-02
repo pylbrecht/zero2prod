a4fb102b421c411d71330bda9b60b6935a86da1c
pylbrecht <pylbrecht@mailbox.org>
2023-11-02 14:22:39 +0100

    Integrate postgres connection in main()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 //! main.rs
 
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect Postgres.");
+    run(listener, connection)?.await
 }
