7508106b1c012c5303954cac8df95087b4329ca0
pylbrecht <pylbrecht@mailbox.org>
2023-11-03 14:26:26 +0100

    Set up env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! main.rs
 
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -7,6 +8,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
