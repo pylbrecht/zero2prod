ef078e6d5abaf2716d48ea9eb8622c23a4e4ac42
pylbrecht <pylbrecht@mailbox.org>
2023-11-05 13:02:37 +0100

    Integrate secrecy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! main.rs
+use secrecy::ExposeSecret;
 
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -14,8 +15,9 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect Postgres.");
     run(listener, connection_pool)?.await
 }
