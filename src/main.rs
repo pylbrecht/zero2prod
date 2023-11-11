49388666f0f3be71e60c76172de495e352653cd3
pylbrecht <pylbrecht@mailbox.org>
2023-11-11 11:15:43 +0100

    Decreasea postgres connection timeout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! main.rs
 use secrecy::ExposeSecret;
 
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -13,10 +13,14 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect Postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect Postgres.");
     run(listener, connection_pool)?.await
 }
