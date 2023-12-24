64d08cc9bf91856b190549f1be79c370530ca7b4
pylbrecht <pylbrecht@mailbox.org>
2023-12-24 10:30:27 +0100

    Share setup code in app and tests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 //! main.rs
 
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::build;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -10,7 +10,7 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let server = build(configuration).await?;
-    server.await?;
+    let app = Application::build(configuration).await?;
+    app.run_until_stopped().await?;
     Ok(())
 }
