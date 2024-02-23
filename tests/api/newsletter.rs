4e0cc575c5d77ecf542ee8772a582029679af9fa
pylbrecht <pylbrecht@mailbox.org>
2024-02-23 21:29:31 +0100

    Add test for rejecting invalid password

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -169,3 +169,31 @@ async fn non_existing_user_is_rejected() {
         response.headers()["WWW-Authenticate"]
     );
 }
+
+#[tokio::test]
+async fn invalid_password_is_rejected() {
+    let app = spawn_app().await;
+    let username = &app.test_user.username;
+    let password = Uuid::new_v4().to_string();
+    assert_ne!(app.test_user.password, password);
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as html</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
