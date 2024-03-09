6108d3220eaa7f997e864ecaa2b5f201052111d8
pylbrecht <pylbrecht@mailbox.org>
2024-03-09 05:16:20 +0100

    Add admin dashboard

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
 //! src/routes/mod.rs
+mod admin;
 mod health_check;
 mod home;
 mod login;
@@ -6,9 +7,10 @@ mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
 
+pub use admin::*;
 pub use health_check::*;
 pub use home::*;
+pub use login::*;
 pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
-pub use login::*;
--- src/startup.rs
+++ src/startup.rs
@@ -15,7 +15,7 @@ use tracing_actix_web::TracingLogger;
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
-    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe
 };
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
@@ -102,6 +102,7 @@ pub async fn run(
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
             .route("/login", web::post().to(login))
+            .route("/admin/dashboard", web::get().to(admin_dashboard))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
